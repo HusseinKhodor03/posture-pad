7d50d05b762c018633a62520d69bccedb6846db1
Hussein Khodor <hkhodor03@gmail.com>
2026-07-19 20:54:16 -0700

    Wrap server startup in main function

--- software/server/main.js
+++ software/server/main.js
@@ -11,27 +11,31 @@ import {
 import { TcpSensorServer } from "./src/network/tcp-sensor-server.js";
 import { WebSocketHub } from "./src/network/web-socket-hub.js";
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const publicDir = path.join(__dirname, ...FRONTEND_PUBLIC_PATH);
+function main() {
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const publicDir = path.join(__dirname, ...FRONTEND_PUBLIC_PATH);
 
-const app = createServerApp({
-  publicDir,
-  frontendUrl: FRONTEND_URL,
-});
+  const app = createServerApp({
+    publicDir,
+    frontendUrl: FRONTEND_URL,
+  });
 
-const httpServer = createServer(app);
-const webSocketHub = new WebSocketHub(httpServer);
-webSocketHub.init();
+  const httpServer = createServer(app);
+  const webSocketHub = new WebSocketHub(httpServer);
+  webSocketHub.init();
 
-httpServer.listen(HTTP_PORT, "0.0.0.0", () => {
-  console.log(`HTTP and WebSocket server listening on port ${HTTP_PORT}`);
-});
+  httpServer.listen(HTTP_PORT, "0.0.0.0", () => {
+    console.log(`HTTP and WebSocket server listening on port ${HTTP_PORT}`);
+  });
 
-const tcpSensorServer = new TcpSensorServer({
-  port: TCP_PORT,
-  onSensorData: (deviceId, sensorData) => {
-    webSocketHub.broadcastSensorData(deviceId, sensorData);
-  },
-});
-tcpSensorServer.listen();
+  const tcpSensorServer = new TcpSensorServer({
+    port: TCP_PORT,
+    onSensorData: (deviceId, sensorData) => {
+      webSocketHub.broadcastSensorData(deviceId, sensorData);
+    },
+  });
+  tcpSensorServer.listen();
+}
+
+main();
